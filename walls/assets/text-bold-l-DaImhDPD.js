46a2d6726757850295cf07ec68ea89686573c70a
alibhtty <77213825+alibhtty@users.noreply.github.com>
2024-10-15 15:31:36 +0200

    3.14

---
+++ walls/assets/text-bold-l-DaImhDPD.js
@@ -0,0 +1,2 @@
+import{j as s}from"./index-xWVz3D8M.js";const e="_text_9zuqj_1",x={text:e};function n({children:t}){return s.jsx("p",{className:x.text,children:t})}export{n as T};
+//# sourceMappingURL=text-bold-l-DaImhDPD.js.map
