28811dd00bd22fde684b80a18fd6dd918ed01946
alibhtty <77213825+alibhtty@users.noreply.github.com>
2025-02-03 23:16:19 +0100

    3.16

--- walls/assets/text-bold-l-BURaB4hB.js
+++
@@ -1,2 +0,0 @@
-import{j as s}from"./index-BKZJ9Os_.js";const e="_text_9zuqj_1",x={text:e};function n({children:t}){return s.jsx("p",{className:x.text,children:t})}export{n as T};
-//# sourceMappingURL=text-bold-l-BURaB4hB.js.map
