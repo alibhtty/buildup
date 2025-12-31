a0ddb15ae663f5c5471eea5ac5708d8528d56ea0
alibhtty <77213825+alibhtty@users.noreply.github.com>
2025-12-31 02:48:08 +0100

    3.07

--- timesburg/sant-pau/source/00_header.js
+++
@@ -1,7 +0,0 @@
-// Variables used by Scriptable.
-// icon-color: deep-blue; icon-glyph: calendar-alt;
-
-// ===========================
-//   HORARIOTIMES – Sant Pau
-//   4Bdev® – Ali Bhtty
-// ===========================
--- timesburg/sant-pau/source/05_layout.js
+++
@@ -1,4 +0,0 @@
-function tieneTurnoValido(trab, dia, turno) {
-  const t = trab.horarios?.[dia]?.[turno];
-  return t && t[0] && t[1];
-}
